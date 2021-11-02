9e246176e1830ddee5f0c8a784906d5973465672
Aminu Usman <66431778+AKMimi@users.noreply.github.com>
2021-11-02 19:45:46 +0100

    robofriends

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+  {
+    id: 1,
+    name: "AK 47",
+    username: "AK",
+    email: "ak@gmail.com",
+  },
+  {
+    id: 2,
+    name: "Maryam Umar",
+    username: "Mimi",
+    email: "mimi@gmail.com",
+  },
+  {
+    id: 3,
+    name: "Maryam Abubakar",
+    username: "Mamma",
+    email: "mamma@gmail.com",
+  },
+  {
+    id: 4,
+    name: "Aisha Abubakar",
+    username: "Ummi",
+    email: "ummi@gmail.com",
+  },
+  {
+    id: 5,
+    name: "Raji Usman",
+    username: "Arrested",
+    email: "arrested@gmail.com",
+  },
+  {
+    id: 6,
+    name: "Aminu Usman",
+    username: "Muchis",
+    email: "muchis@gmail.com",
+  },
+  {
+    id: 7,
+    name: "Mujahid Usman",
+    username: "Mujis",
+    email: "mujis@gmail.com",
+  },
+  {
+    id: 8,
+    name: "Zainab Usman",
+    username: "Zee",
+    email: "zee@gmail.com",
+  },
+  {
+    id: 9,
+    name: "Aisha Usman",
+    username: "Black",
+    email: "black@gmail.com",
+  },
+  {
+    id: 10,
+    name: "Master Usman",
+    username: "Master",
+    email: "master@gmail.com",
+  },
+];
